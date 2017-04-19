3f2f54d25505365cc28cd52c80dc1d40d312ed48
alexgriffen <alexgriffen@alexgriffensmbp>
2017-04-19 08:09:19 -0700

    Update HStest.java

--- src/main/java/com/hellosign/apisupport/hellosigntest/HStest.java
+++ src/main/java/com/hellosign/apisupport/hellosigntest/HStest.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.hellosign.apisupport.hellosigntest;
 
 import com.hellosign.sdk.HelloSignClient;
